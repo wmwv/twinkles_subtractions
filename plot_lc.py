e452a242b87537f224410d679efa88a6ba099fa1
Michael Wood-Vasey <wmwv@pitt.edu>
2016-11-15 19:52:37 -0800

    Add basic plotting program.

--- plot_lc.py
+++ plot_lc.py
@@ -9,10 +9,22 @@ from astropy.table import Table
 if __name__ == "__main__":
     plot_file = sys.argv[1]
 
+    plot_mag = True
+    plot_flux = True
+
     data = Table.read(plot_file)
 
-    plt.errorbar(data['mjd'], data['base_PsfFlux_flux'], data['base_PsfFlux_fluxSigma'], linestyle='none')
-    plt.xlabel('MJD')
-    plt.ylabel('base_PsfFlux_flux')
+    if plot_mag:
+        plt.errorbar(data['mjd'], data['base_PsfFlux_mag'], data['base_PsfFlux_magSigma'], linestyle='none')
+        plt.xlabel('MJD')
+        plt.ylabel('base_PsfFlux_mag')
+        ylim = plt.ylim()
+        plt.ylim(max(ylim), min(ylim))
+        plt.show()
 
-    plt.show()
+    if plot_flux:
+        plt.errorbar(data['mjd'], data['base_PsfFlux_flux_zp25'], data['base_PsfFlux_fluxSigma_zp25'], linestyle='none')
+        plt.xlabel('MJD')
+        plt.ylabel('flux_zp25  [mag = -2.5*log10(flux) + 25]')
+        plt.show()
+
